5e8fbdb535f6777e302e8808f0c9e42a37f4227b
srinathlee <iamsrinath5255@gmail.com>
2022-01-15 09:59:52 +0530

    commited two files

---
+++ srinath.js
@@ -0,0 +1,6 @@
+
+let img="https://res.cloudinary.com/dldcgj0mx/image/upload/v1607679131/IMG_20200927_121854_075_et5r9f.jpg";
+let ele=document.getElementsByTagName("img");
+for (let i of ele){
+    i.src=img;
+}
